bda2f0d353f3b0d86af8579d6d0ccb9771655bbd
Taylor <taymosier@gmail.com>
2019-04-22 15:49:46 -0400

    Updates

---
+++ precache-manifest.2c78bf23aee10933e7a8c2996c694cb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6267b80c9bef42b0d34bb3d3944af69",
+    "url": "/portfolio/static/media/canoe.d6267b80.png"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "bc3d27fc589208cb20b5",
+    "url": "/portfolio/static/js/main.3839ca2d.chunk.js"
+  },
+  {
+    "revision": "53b5710b0288939fcf82",
+    "url": "/portfolio/static/js/2.99f75d33.chunk.js"
+  },
+  {
+    "revision": "bc3d27fc589208cb20b5",
+    "url": "/portfolio/static/css/main.7dddc343.chunk.css"
+  },
+  {
+    "revision": "53b5710b0288939fcf82",
+    "url": "/portfolio/static/css/2.f389e732.chunk.css"
+  },
+  {
+    "revision": "2b6a395b0d1c84b9e24f3c73d9ba7ab1",
+    "url": "/portfolio/index.html"
+  }
+];
